eebc93fc4f08021a90aa4665f215bac754693b7b
Himanshu <himanshu21464@iiitd.ac.in>
2022-09-24 21:02:15 +0530

    update

--- src/Future_Builder.java
+++ src/Future_Builder.java
@@ -3,25 +3,31 @@ import java.util.*;
 
 public class Future_Builder {
     public static void main_menu(){
+        System.out.println("-----------------------------------------------------");
         System.out.println("Welcome to Future Builder");
         System.out.println("1. Enter the Application");
         System.out.println("2. Exit the Application");
+        System.out.println("-----------------------------------------------------");
         System.out.print("Enter your choice: ");
         Scanner sc =new Scanner(System.in);
         int option=sc.nextInt();
+        System.out.println(" ");
         if(option==1){
             Future_Builder.Application();
         } else if (option==2){
             System.out.println("Thanks for using FutureBuilder!!!");
+            System.out.println("*************************************************");
             java.lang.System.exit(0);
         }
         else {
             System.out.println("Wrong Input!!");
+            System.out.println(" ");
             main_menu();
         }
     }
 
     public static void Application() {
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++");
         System.out.println("1. Enter as Student Mode");
         System.out.println("2. Enter as Company Mode");
         System.out.println("3. Enter as Placement Cell Mode");
@@ -29,6 +35,7 @@ public class Future_Builder {
         System.out.print("Choose the mode you want to Enter in: ");
         Scanner sc =new Scanner(System.in);
         int mode= sc.nextInt();
+        System.out.println(" ");
         if (mode==1){
             Students.function();
         } else if (mode==2){
@@ -41,6 +48,7 @@ public class Future_Builder {
             main_menu();
         } else {
             System.out.println("Wrong Input");
+            System.out.println(" ");
             Application();
         }
 
--- src/Students.java
+++ src/Students.java
@@ -3,20 +3,23 @@ import java.util.Scanner;
 public class Students{
 
         public static void function(){
+                System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
                 System.out.println("1. Enter as Student");
                 System.out.println("2. Add Students");
                 System.out.println("3. Back");
                 System.out.println("Choose the Student query to perform: ");
                 Scanner sc=new Scanner(System.in);
                 int query=sc.nextInt();
+                System.out.println(" ");
                 if(query==1){
-                        Enter_As_Student.student_mode();
+                        Enter_As_Student.credentials();
                 } else if (query==2) {
                         Add_Students.Student_function();
                 } else if (query==3) {
                         Future_Builder.Application();
                 }  else{
                         System.out.println("Wrong Input!!");
+                        System.out.println(" ");
                         Students.function();
 
                 }
@@ -27,13 +30,18 @@ public class Students{
 
 }
 class Enter_As_Student {
-        public static void student_mode(){
+        public static void credentials(){
                 Scanner sc =new Scanner(System.in);
                 System.out.print("Enter your name: ");
                 String name=sc.nextLine();
                 System.out.print("Enter your Roll no.: ");
                 int roll_no=sc.nextInt();
-                System.out.println("Welcome "+name+"!!");
+                System.out.println("-------------- Welcome "+name+"!! -----------------");
+
+                student_mode();
+        }
+        public static void student_mode(){
+                Scanner sc =new Scanner(System.in);
                 System.out.println("1. Register for Placement Drive");
                 System.out.println("2. Register for Company");
                 System.out.println("3. Get All available companies");
@@ -44,6 +52,7 @@ class Enter_As_Student {
                 System.out.println("8. Back");
                 System.out.print("Enter your choice: ");
                 int choice=sc.nextInt();
+                System.out.println("----------------------------------------------------");
                 if(choice==1){
 
                 } else if (choice==2) {
@@ -63,6 +72,8 @@ class Enter_As_Student {
 
                 }else {
                         System.out.println("Wrong Input");
+                        System.out.println(" ");
+                        student_mode();
                 }
 
 
